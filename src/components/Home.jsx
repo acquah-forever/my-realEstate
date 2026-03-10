0498b5bd27883289f0a30b0c898cae55d502ef74
acquah-forever <kojoaddo598@gmail.com>
2026-03-10 13:24:24 +0000

    added a new listing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Hero from './Hero'
-import ListingForTheDay from './ListingForTheDay'
+import Listing1 from './Listing1'
+import Listing2 from './Listing2'
 import Interior from '../assets/buildings/interior.jpg'
+import Interior2 from '../assets/buildings/interior2.jpg'
+
 
 
 const Home = () => {
@@ -11,7 +14,13 @@ const Home = () => {
       <div className='h-screen relative bg-fixed bg-cover bg-center' style={{ backgroundImage: `url(${Interior})` }}>
         <div className='absolute bg-black/20 inset-0'></div>
         <div className='relative z-10'>
-          <ListingForTheDay />
+          <Listing1 />
+        </div>
+      </div>
+      <div className='h-screen relative bg-fixed bg-cover bg-center' style={{ backgroundImage: `url(${Interior2})` }}>
+        <div className='absolute bg-black/20 inset-0'></div>
+        <div className='relative z-10'>
+          <Listing2 />
         </div>
       </div>
 
