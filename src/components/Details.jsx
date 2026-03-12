6618d439e62f7fa78a610dd5b896003e80cc695b
acquah-forever <kojoaddo598@gmail.com>
2026-03-12 19:30:50 +0000

    finalosed Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const Details = () => {
     return (
-        <div className='p-10 grid grid-cols-2'>
+        <div className='p-10 grid md:flex md:gap-20'>
             <div className='mt-15'>
                 <h1 className='text-xl'>Exclusive Guidaance for</h1>
                 <h1 className='mt-10 text-3xl font-semibold'>Buyers, Sellers & Investors</h1>
             </div>
 
-            <div className='mt-15'>
+            <div className='mt-15 tracking-tighter'>
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex gap-15'>
-                        <h1 className='text-xl'>01.</h1>
-                        <h1 className='text-xl uppercase'>Buying</h1>
-                        <h1 className='text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-md md:text-xl'>01.</h1>
+                        <h1 className='text-md md:text-xl uppercase'>Buying</h1>
+                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex gap-15'>
-                        <h1 className='text-xl'>02.</h1>
-                        <h1 className='text-xl uppercase'>Selling</h1>
-                        <h1 className='text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-md md:text-xl'>02.</h1>
+                        <h1 className='text-md md:text-xl uppercase'>Selling</h1>
+                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
@@ -31,9 +31,9 @@ const Details = () => {
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex gap-15'>
-                        <h1 className='text-xl'>03.</h1>
-                        <h1 className='text-xl uppercase'>Leasing</h1>
-                        <h1 className='text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-md md:text-xl'>03.</h1>
+                        <h1 className='text-md md:text-xl uppercase'>Leasing</h1>
+                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
@@ -41,9 +41,9 @@ const Details = () => {
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex gap-10'>
-                        <h1 className='text-xl'>04.</h1>
-                        <h1 className='text-xl uppercase'>Investment</h1>
-                        <h1 className='text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-md md:text-xl'>04.</h1>
+                        <h1 className='text-md md:text-xl uppercase'>Investment</h1>
+                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
             </div>
