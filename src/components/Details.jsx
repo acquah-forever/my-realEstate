64db98284ca11780ace2da6e4e014fe6bd25dfe1
acquah-forever <kojoaddo598@gmail.com>
2026-03-12 13:59:34 +0000

    edited details in Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 const Details = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <h1>Exclusive Guidaance for</h1>
+                <h1 className='text-3xl font-semibold'>Buyers, Sellers & Investors</h1>
+            </div>
+            <div>
+                <div className='border-2'></div>
+                <div>
+                    <h1>01.</h1>
+                    <h1>Buying</h1>
+                </div>
+                <h1>Skip endless searches. We leverage exclusive access
+                    and expert negotiation to pinpoint your ideal home,
+                    ensuring a smooth, swift purchase. Your next chapter
+                    begins today.
+                </h1>
+            </div>
+
+        </div>
+    )
 }
 
 export default Details
