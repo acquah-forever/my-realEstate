e03adf0e0d2620c0460c5a870a4323a255b533b1
acquah-forever <kojoaddo598@gmail.com>
2026-03-12 14:07:40 +0000

    added grid

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 
 const Details = () => {
     return (
-        <div>
+        <div className='grid grid-cols-2'>
             <div>
                 <h1 className='text-xl'>Exclusive Guidaance for</h1>
                 <h1 className='text-3xl font-semibold'>Buyers, Sellers & Investors</h1>
             </div>
+
             <div>
+                <div>
+                    <div className='border-2'></div>
+                    <div>
+                        <h1>01.</h1>
+                        <h1>Buying</h1>
+                    </div>
+                    <h1>Skip endless searches. We leverage exclusive access
+                        and expert negotiation to pinpoint your ideal home,
+                        ensuring a smooth, swift purchase. Your next chapter
+                        begins today.
+                    </h1>
+
+                </div>
+
                 <div className='border-2'></div>
                 <div>
-                    <h1>01.</h1>
-                    <h1>Buying</h1>
+                    <h1>02.</h1>
+                    <h1>Selling</h1>
                 </div>
                 <h1>Skip endless searches. We leverage exclusive access
                     and expert negotiation to pinpoint your ideal home,
@@ -19,40 +34,28 @@ const Details = () => {
                     begins today.
                 </h1>
 
-            </div>
+                <div className='border-2'></div>
+                <div>
+                    <h1>03.</h1>
+                    <h1>Investing</h1>
+                </div>
+                <h1>Skip endless searches. We leverage exclusive access
+                    and expert negotiation to pinpoint your ideal home,
+                    ensuring a smooth, swift purchase. Your next chapter
+                    begins today.
+                </h1>
 
-            <div className='border-2'></div>
-            <div>
-                <h1>02.</h1>
-                <h1>Selling</h1>
+                <div className='border-2'></div>
+                <div>
+                    <h1>04.</h1>
+                    <h1>Leasing</h1>
+                </div>
+                <h1>Skip endless searches. We leverage exclusive access
+                    and expert negotiation to pinpoint your ideal home,
+                    ensuring a smooth, swift purchase. Your next chapter
+                    begins today.
+                </h1>
             </div>
-            <h1>Skip endless searches. We leverage exclusive access
-                and expert negotiation to pinpoint your ideal home,
-                ensuring a smooth, swift purchase. Your next chapter
-                begins today.
-            </h1>
-
-            <div className='border-2'></div>
-            <div>
-                <h1>03.</h1>
-                <h1>Investing</h1>
-            </div>
-            <h1>Skip endless searches. We leverage exclusive access
-                and expert negotiation to pinpoint your ideal home,
-                ensuring a smooth, swift purchase. Your next chapter
-                begins today.
-            </h1>
-
-            <div className='border-2'></div>
-            <div>
-                <h1>04.</h1>
-                <h1>Leasing</h1>
-            </div>
-            <h1>Skip endless searches. We leverage exclusive access
-                and expert negotiation to pinpoint your ideal home,
-                ensuring a smooth, swift purchase. Your next chapter
-                begins today.
-            </h1>
         </div>
     )
 }
