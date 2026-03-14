e20c016cf286cddd81e6d925b119b74d41e76c7e
acquah-forever <kojoaddo598@gmail.com>
2026-03-14 14:36:23 +0000

    increase the height of amber cards to h-120

--- src/MoreInfo.jsx
+++ src/MoreInfo.jsx
@@ -6,36 +6,35 @@ const MoreInfo = () => {
     return (
         <div className='p-10 grid grid-cols-1 '>
             <h1 className='mb-12 text-center text-3xl font-semibold'>A Curated Portfolio of Homes</h1>
-            <div className='
-            flex justify-between items-center space-x-10'>
-                <div className='bg-amber-400/50 p-7 max-w-2xl'>
+            <div className='flex justify-between items-center space-x-10'>
+                <div className='bg-amber-400/50 p-7 max-w-2xl h-120'>
                     <div className='mb-5 flex justify-between'>
                         <h1 className='uppercase text-2xl'>Vision & Strategy</h1>
                         <House className='w-10 h-10' />
                     </div>
-                    <div className='border-t-2 mb-20'></div>
+                    <div className='border-t-2 mb-50'></div>
                     <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
                 </div>
 
-                <div className='bg-amber-400/50 p-7 max-w-2xl'>
+                <div className='bg-amber-400/50 p-7 max-w-2xl h-120'>
                     <div className='mb-5 flex justify-between'>
                         <h1 className='uppercase text-2xl'>Targeted Action</h1>
                         <Goal className='w-10 h-10' />
                     </div>
-                    <div className='border-t-2 mb-20'></div>
+                    <div className='border-t-2 mb-50'></div>
                     <h3 className='text-lg'>We leverage our network and expertise to quickly identify opportunities, handle negotiations skillfully, and guide you seamlessly through each milestone.</h3>
                 </div>
 
-                <div className='bg-amber-400/50 p-7 max-w-2xl'>
+                <div className='bg-amber-400/50 p-7 max-w-2xl h-120'>
                     <div className='mb-5 flex justify-between'>
                         <h1 className='uppercase text-2xl'>Close and Live!</h1>
                         <Smile className='w-10 h-10' />
                     </div>
-                    <div className='border-t-2 mb-20'></div>
+                    <div className='border-t-2 mb-50'></div>
                     <h3 className='text-lg'>With all the details managed precisely, we swiftly move to closing, getting you into your new home or successful investment, so you can truly start living.</h3>
                 </div>
             </div>
-            <button className='bg-pink-400 p-5 mx-auto rounded-lg mt-7'>Get Free Apointment</button>
+            <button className='text-white text-2xl bg-pink-400 p-5 mx-auto rounded-lg mt-7'>Get Free Apointment</button>
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className='text-white space-y-10'>
         <h1 className='text-6xl sm:text-7xl md:text-8xl text-amber-200'>Find Your Dream Home</h1>
         <h2 className='text-3xl text-white'>We understand the fact that mordern people strive for maximum comfort</h2>
-         <button className="btn btn-secondary text-2xl sm:text-3xl md:text-4xl p-10">Get Free Consultation</button>
+         <button className="btn btn-secondary text-2xl sm:text-3xl md:text-4xl p-10">Get Free Appointment</button>
       </div>
 
     </div>
