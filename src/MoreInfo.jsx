d13c02713973a73040d145973c109f8f20a70225
acquah-forever <kojoaddo598@gmail.com>
2026-03-14 12:57:12 +0000

    worked on media queries

--- src/MoreInfo.jsx
+++ src/MoreInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { House } from 'lucide-react'
+import { House,Goal,Smile } from 'lucide-react'
 
 const MoreInfo = () => {
     return (
@@ -15,20 +15,20 @@ const MoreInfo = () => {
 
             <div className='bg-amber-400/50 p-7 max-w-2xl'>
                 <div className='mb-5 flex justify-between'>
-                    <h1 className='uppercase text-2xl'>Vision & Strategy Session</h1>
-                    <House className='w-10 h-10' />
+                    <h1 className='uppercase text-2xl'>Targeted Action</h1>
+                    <Goal className='w-10 h-10' />
                 </div>
                 <div className='border-t-2 mb-20'></div>
-                <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
+                <h3 className='text-lg'>We leverage our network and expertise to quickly identify opportunities, handle negotiations skillfully, and guide you seamlessly through each milestone.</h3>
             </div>
 
             <div className='bg-amber-400/50 p-7 max-w-2xl'>
                 <div className='mb-5 flex justify-between'>
-                    <h1 className='uppercase text-2xl'>Vision & Strategy Session</h1>
-                    <House className='w-10 h-10' />
+                    <h1 className='uppercase text-2xl'>Close and Live!</h1>
+                    <Smile className='w-10 h-10' />
                 </div>
                 <div className='border-t-2 mb-20'></div>
-                <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
+                <h3 className='text-lg'>With all the details managed precisely, we swiftly move to closing, getting you into your new home or successful investment, so you can truly start living.</h3>
             </div>
         </div>
     )
