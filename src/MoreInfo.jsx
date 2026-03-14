6b1c3d16104dd2ed63339888eac88726b74fbd81
acquah-forever <kojoaddo598@gmail.com>
2026-03-14 12:51:39 +0000

    added lucide icons

--- src/MoreInfo.jsx
+++ src/MoreInfo.jsx
@@ -4,34 +4,31 @@ import { House } from 'lucide-react'
 const MoreInfo = () => {
     return (
         <div className='p-7 flex justify-between space-x-10'>
-            <div className='bg-amber-400/50 p-7 border-2 max-w-2xl'>
-                <div className='flex'>
-                    <House />
-                    <h1>A curated Portfolio of Homes</h1>
+            <div className='bg-amber-400/50 p-7 max-w-2xl'>
+                <div className='mb-5 flex justify-between'>
+                    <h1 className='uppercase text-2xl'>Vision & Strategy Session</h1>
+                    <House className='w-10 h-10' />
                 </div>
-                <h1>Vision and Strategy</h1>
-                <div></div>
-                <h3>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
+                <div className='border-t-2 mb-20'></div>
+                <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
             </div>
 
-            <div className='p-7 bg-amber-400/50 border-2 max-w-2xl'>
-                <div className='flex'>
-                    <House />
-                    <h1>A curated Portfolio of Homes</h1>
+            <div className='bg-amber-400/50 p-7 max-w-2xl'>
+                <div className='mb-5 flex justify-between'>
+                    <h1 className='uppercase text-2xl'>Vision & Strategy Session</h1>
+                    <House className='w-10 h-10' />
                 </div>
-                <h1>Vision and Strategy</h1>
-                <div></div>
-                <h3>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
+                <div className='border-t-2 mb-20'></div>
+                <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
             </div>
 
-            <div className='p-7 bg-amber-400/50 border-2 max-w-2xl'>
-                <div className='flex'>
-                    <House />
-                    <h1>A curated Portfolio of Homes</h1>
+            <div className='bg-amber-400/50 p-7 max-w-2xl'>
+                <div className='mb-5 flex justify-between'>
+                    <h1 className='uppercase text-2xl'>Vision & Strategy Session</h1>
+                    <House className='w-10 h-10' />
                 </div>
-                <h1>Vision and Strategy</h1>
-                <div></div>
-                <h3>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
+                <div className='border-t-2 mb-20'></div>
+                <h3 className='text-lg'>We start with a focused conversation to understand your exact goals and craft a personalized, direct strategy to achieve them without delay.</h3>
             </div>
         </div>
     )
