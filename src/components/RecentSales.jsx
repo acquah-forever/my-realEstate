1f4347afa332cb4b7505d02ec2599a8fcd80f7de
acquah-forever <kojoaddo598@gmail.com>
2026-03-22 14:18:18 +0000

    added dimensions to houses

--- src/components/RecentSales.jsx
+++ src/components/RecentSales.jsx
@@ -16,12 +16,30 @@ const RecentSales = () => {
             <div className='flex'>
                 <div>
                     <img className='w-98 h-221 object-cover' src={House1} alt='house1' />
+                    <h1>GREENWICH VILLAGE TOWNHOUSE</h1>
+                    <h2>a classic pre-war renovated property in an exclusive neighborhood</h2>
+                    <div></div>
+                    <h1>2 Bed</h1>
+                    <h1>2 Bath</h1>
+                    <h1>165 sqm</h1>
                 </div>
                 <div>
                     <img className='w-100 h-221 object-cover' src={House2} alt='house2' />
+                    <h1>DOWNTOWN SKYLINE PENTHOUSE</h1>
+                    <h2>Luxurious living with breathtaking cityscapes from every room</h2>
+                    <div></div>
+                    <h1>3 Bed</h1>
+                    <h1>2.5 Bath</h1>
+                    <h1>190 sqm</h1>
                 </div>
                 <div>
                     <img className='w-md h-221 object-cover' src={House3} alt='house3' />
+                    <h1>MODERN CITY LOFT WITH TERRACE</h1>
+                    <h2>Elegant design and urban serenity with a private outdoor space.</h2>
+                    <div></div>
+                    <h1>3 Bed</h1>
+                    <h1>2 Bath</h1>
+                    <h1>167 sqm</h1>
                 </div>
 
 
