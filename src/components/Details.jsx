f598e123901aabb52c26d19569b4e0703bb1d47e
acquah-forever <kojoaddo598@gmail.com>
2026-03-22 13:43:24 +0000

    updated Details.jsx text size

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -12,18 +12,18 @@ const Details = () => {
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex flex-col gap-4 md:flex-row md:gap-10 '>
-                        <h1 className='text-md md:text-xl'>01.</h1>
-                        <h1 className='text-md md:text-xl uppercase'>Buying</h1>
-                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-lg md:text-xl'>01.</h1>
+                        <h1 className='text-lg md:text-xl uppercase'>Buying</h1>
+                        <h1 className='text-lg md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex flex-col gap-4 md:flex-row md:gap-10'>
-                        <h1 className='text-md md:text-xl'>02.</h1>
-                        <h1 className='text-md md:text-xl uppercase'>Selling</h1>
-                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-lg md:text-xl'>02.</h1>
+                        <h1 className='text-lg md:text-xl uppercase'>Selling</h1>
+                        <h1 className='text-lg md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
@@ -31,9 +31,9 @@ const Details = () => {
                 <div>
                     <div className='border'></div>
                     <div className='p-5 flex flex-col gap-3 md:flex-row md:gap-10'>
-                        <h1 className='text-md md:text-xl'>03.</h1>
-                        <h1 className='text-md md:text-xl uppercase'>Leasing</h1>
-                        <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
+                        <h1 className='text-lg md:text-xl'>03.</h1>
+                        <h1 className='text-lg md:text-xl uppercase'>Leasing</h1>
+                        <h1 className='text-lg md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
 
