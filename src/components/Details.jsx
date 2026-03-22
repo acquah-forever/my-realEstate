abee4d5d9be01b924b7ccc03f50bc1d2a183c4fc
acquah-forever <kojoaddo598@gmail.com>
2026-03-22 13:44:46 +0000

    editing Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,7 +4,7 @@ const Details = () => {
     return (
         <div className='p-10 grid md:flex md:gap-20'>
             <div className='mt-15'>
-                <h1 className='text-xl'>Exclusive Guidaance for</h1>
+                <h1 className='text-xl md:text-2xl'>Exclusive Guidaance for</h1>
                 <h1 className='mt-10 text-3xl font-semibold'>Buyers, Sellers & Investors</h1>
             </div>
 
@@ -42,7 +42,7 @@ const Details = () => {
                     <div className='border'></div>
                     <div className='p-5 flex flex-col gap-4 md:flex-row md:gap-10'>
                         <h1 className='text-md md:text-xl'>04.</h1>
-                        <h1 className='text-md md:text-xl uppercase'>Investment</h1>
+                        <h1 className='text-md md:text-xl uppercase'>Renting</h1>
                         <h1 className='text-md md:text-xl'>Skip endless searches. We leverage exclusive access and expert negotiation to pinpoint your ideal home, ensuring a smooth, swift purchase. Your next chapter begins today.</h1>
                     </div>
                 </div>
