9ac3eb4be6d5a07721db2b1bb8ab460a09c98682
acquah-forever <kojoaddo598@gmail.com>
2026-03-22 13:59:48 +0000

    edited house images

--- src/components/RecentSales.jsx
+++ src/components/RecentSales.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import House1 from '../assets/buildings/house1.jpg'
 import House2 from '../assets/buildings/house2.jpg'
+import House3 from '../assets/buildings/house3.jpg'
 
 const RecentSales = () => {
     return (
@@ -12,11 +13,17 @@ const RecentSales = () => {
                 </div>
             </div>
 
-            <div>
+            <div className='flex'>
                 <div>
-                    <img className='' src={House1} alt='house1' />
-
+                    <img className='w-98 h-auto object-cover' src={House1} alt='house1' />
                 </div>
+                <div>
+                    <img className='w-100 h-auto object-cover' src={House2} alt='house2' />
+                </div>
+                <div>
+                    <img className='w-md h-auto object-cover' src={House3} alt='house3' />
+                </div>
+
 
             </div>
 
