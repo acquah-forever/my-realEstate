fd67477e3b88a20efe2f7708e5716d1bac19cffe
acquah-forever <kojoaddo598@gmail.com>
2026-03-24 12:26:33 +0000

    changed font style

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,13 +3,13 @@ import { Star } from "lucide-react"
 
 const Reviews = ({ icon, comment, name }) => {
   return (
-    <div className="bg-white p-6 rounded-xl shadow-md space-y-5 flex flex-col items-center text-center h-full">
+    <div className="bg-white p-5 rounded-xl shadow-md space-y-5 flex flex-col items-center text-center h-full">
 
       <span className="text-amber-500 text-3xl flex">{icon}</span>
 
-      <p className="text-gray-600">{comment}</p>
+      <p className="text-gray-600 font-semibold">{comment}</p>
 
-      <h3 className="font-semibold text-gray-800">{name}</h3>
+      <h3 className="font-bold text-gray-800">{name}</h3>
 
     </div>
   );
