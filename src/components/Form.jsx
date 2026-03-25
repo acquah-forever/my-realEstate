9aaf55e6072c4f5354dcd0b0660b611cb4689325
acquah-forever <kojoaddo598@gmail.com>
2026-03-25 20:22:44 +0000

    edited main heading

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,12 @@
 import React, { useForm } from 'react'
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
+    // const { register, handleSubmit, formState: { errors } } = useForm()
 
     return (
-        <div>
-            <form>
-                <h1>Ready to stop Searching and star Living?</h1>
+        <div className='p-15'>
+            <form className='p-10 bg-amber-200 max-w-7xl mx-auto'>
+                <h1>Ready to stop Searching and Start Living?</h1>
                 <p>Reach out today for a focused discussion on your real estate goals.</p>
                 <p>We are ready to make it happen.</p>
                 <label>
@@ -18,6 +18,7 @@ const Form = () => {
                     <input type="number" />
                     <h1>Email*</h1>
                     <input type="email" />
+                    <h1>Services*</h1>
                 </label>
             </form>s
         </div>
