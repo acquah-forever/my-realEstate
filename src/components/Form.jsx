a4e97675e39340dfe66f5d409a08e6d7983a6864
acquah-forever <kojoaddo598@gmail.com>
2026-03-25 20:17:24 +0000

    useForm added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useForm } from 'react'
 
 const Form = () => {
-  return (
-    <div>
-        <h1>Ready to stop Searching and star Living?</h1>
-        <p>Reach out today for a focused discussion on your real estate goals.</p>
-        <p>We are ready to make it happen.</p>
-    </div>
-  )
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    return (
+        <div>
+            <form>
+                <h1>Ready to stop Searching and star Living?</h1>
+                <p>Reach out today for a focused discussion on your real estate goals.</p>
+                <p>We are ready to make it happen.</p>
+                <label>
+                    <h1>Name*</h1>
+                    <input type="text" />
+                    <h1>Last name*</h1>
+                    <input type="text" />
+                    <h1>Phone number*</h1>
+                    <input type="number" />
+                    <h1>Email*</h1>
+                    <input type="email" />
+                </label>
+            </form>s
+        </div>
+    )
 }
 
 export default Form
