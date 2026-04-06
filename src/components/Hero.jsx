975534cb4fff13e4504285eefb96cdfbfa9d9bda
acquah-forever <kojoaddo598@gmail.com>
2026-04-06 14:50:31 +0100

    edited background color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {motion} from 'motion/react'
+import { motion } from 'motion/react'
 import Res from '../assets/buildings/hero.jpg'
 
 
 const Hero = () => {
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center text-center  dark:bg-slate-600 bg-cover bg-center bg-fixed' style={{ backgroundImage: `url(${Res})` }} >
-      <div className='absolute inset-0 bg-black/60 -z-50'></div>
-      <div className='text-white space-y-10'>
+    <div className='relative min-h-screen flex flex-col justify-center items-center text-center dark:bg-slate-600 bg-cover bg-center bg-fixed overflow-hidden' style={{ backgroundImage: `url(${Res})` }} >
+      <div className='absolute inset-0 bg-black/20 z-0'></div>
+      <div className='relative z-10 text-white space-y-10'>
         <h1 className='text-6xl sm:text-7xl md:text-8xl text-amber-200'>Find Your Dream Home</h1>
         <h2 className='text-3xl text-white'>We understand the fact that mordern people strive for maximum comfort</h2>
-         <button className="btn btn-secondary text-2xl sm:text-3xl md:text-4xl p-10">Get Free Appointment</button>
+        <button className="btn btn-secondary text-2xl sm:text-3xl md:text-4xl p-10">Get Free Appointment</button>
       </div>
 
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,40 +23,40 @@ const NavBar = () => {
   }
 
   return (
-    <div>
-      <div className='px-7 py-2 backdrop-blur-2xl top-0 left-0 fixed w-full z-50 flex justify-between items-center '>
-        <a href='#' className='text-rose-400 flex items-center space-x-1'>
-          <House className='w-10 h-10' />
-          <h1 className='text-2xl md:text-3xl cursor-pointer'>Kojo Addo</h1>
-        </a>
-        <div>
-          {!user ?
-            <ul className='hidden text-xl text-white bg-black/30 rounded-lg px-5 py-2 sm:flex  space-x-7'>
-              <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to='/'>Home</NavLink></li>
-              <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to="/logIn">Log In</NavLink></li>
-              <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to="/signUp">Sign Up</NavLink></li>
-            </ul>
-            :
-            <h1 className='hidden text-xl text-white bg-black/30 rounded-lg px-5 py-2 sm:flex  space-x-7'>Hello {user.email} &nbsp; <button className='bg-gray-500 p-2 rounded' onClick={logOut}>LogOut</button></h1>
-          }
-          <div className='flex items-center'>
-            <button className='cursor-pointer text-white flex sm:hidden transition-transform ease-in-out duration-200' onClick={toggleMenu}>{menu ? <X /> : <Menu />}</button>
+    <>
+      <div className='mb-10 relative'>
+        <div className=' px-7 py-2 backdrop-blur-2xl top-0 left-0 fixed w-full z-50 flex justify-between items-center '>
+          <NavLink to= '/'>
+            <a className='text-rose-400 flex items-center space-x-1'>
+              <House className='w-10 h-10' />
+              <h1 className='text-2xl md:text-3xl cursor-pointer'>Kojo Addo</h1>
+            </a>
+          </NavLink>
+          <div>
+            {!user ?
+              <ul className='hidden text-xl text-white bg-black/30 rounded-lg px-5 py-2 sm:flex  space-x-7'>
+                <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to='/'>Home</NavLink></li>
+                <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to="/logIn">Log In</NavLink></li>
+                <li className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out"><NavLink to="/signUp">Sign Up</NavLink></li>
+              </ul>
+              :
+              <h1 className='hidden text-xl text-white bg-black/30 rounded-lg px-5 py-2 sm:flex  space-x-7'>Hello {user.email} &nbsp; <button className='bg-gray-500 p-2 rounded' onClick={logOut}>LogOut</button></h1>
+            }
+            <div className='flex items-center'>
+              <button className='cursor-pointer text-white flex sm:hidden transition-transform ease-in-out duration-200' onClick={toggleMenu}>{menu ? <X /> : <Menu />}</button>
+            </div>
           </div>
         </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
       <div>
+
         <AnimatePresence>
           {menu && (
-            <div className='text-white rounded-xl flex justify-center items-center'>
-
+            <div className='text-white fixed z-50 top-20 px-10'>
               {!user ? (
                 <div>
                   <motion.ul variants={parent} initial='hidden' animate='visible' exit={{ opacity: 0, x: -50 }} className='text-2xl flex justify-center space-x-5 sm:hidden'>
-                    <motion.li variants={children} className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out" onClick={() => setMenu(false)}><NavLink to='/'>Home</NavLink></motion.li>
+                    <motion.li variants={children} className="cursor-pointer  inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out" onClick={() => setMenu(false)}><NavLink to='/'>Home</NavLink></motion.li>
                     <motion.li variants={children} className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out" onClick={() => setMenu(false)}><NavLink to='/logIn'>Log In</NavLink></motion.li>
                     <motion.li variants={children} className="cursor-pointer inline-block border-b-2 border-transparent hover:border-white transition-all duration-500 ease-in-out" onClick={() => setMenu(false)}><NavLink to='/signUp'>Sign Up</NavLink></motion.li>
                   </motion.ul>
@@ -70,7 +70,7 @@ const NavBar = () => {
           )}
         </AnimatePresence>
       </div>
-    </div>
+    </>
   )
 }
 
