88bb4610f0d89791e455d759a01951762dede22d
acquah-forever <kojoaddo598@gmail.com>
2026-04-22 11:10:45 +0000

    edited showcase.js

--- src/data/Showcase.js
+++ src/data/Showcase.js
@@ -1,4 +1,5 @@
 const showCase = [
+
     {
         id: 1,
         title: "En Garde City Loft with Skyline View",
@@ -8,13 +9,54 @@ const showCase = [
         bedrooms: 4,
         bathrooms: 2,
         area: "3014 sqft",
-        features: ["Central Heating", "Garage", "Garden", "Double Glazing", "Driveway"],
+        features: ["Centralized Heating", "Garage", "Double Glazing"],
         images: "https://images.unsplash.com/photo-1592839656073-833413ae8874?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         listedDate: "2026-03-20",
         agent: {
             name: "James Walker",
             phone: "+44 7700 900001",
             email: "james.walker@estateuk.com"
+        }
+
+    },
+
+        
+    {
+        id: 2,
+        title: "Lovely Spacious Apartment with Garden View",
+        price: "£420,000",
+        location: "Bristol",
+        description: "A staet-of-the-art build filled with a serene and luscious envronment",
+        bedrooms: 3,
+        bathrooms: 2,
+        area: "2880 sqft",
+        features: ["Open Garden View", "Garage", "Elevators"],
+        images: "https://images.unsplash.com/photo-1647452375084-3ad0db6c9331?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        listedDate: "2026-04-10",
+        agent: {
+            name: "Jack Reacher",
+            phone: "+44 7700 900001",
+            email: "jack.reacher@estateuk.com"
+        }
+
+    },
+
+        {
+        id: 3,
+        title: "Studio Aprtment with Modernistic Fittings",
+        price: "£420,000",
+        location: "Bristol",
+        description: "Studio Apartment suitable for the ever ready student or recently employed.",
+        bedrooms: 1,
+        bathrooms: 1,
+        area: "1200 sqft",
+        features: ["Foyer", "highspeed Internet", "Bike Garage"],
+        images: "https://images.unsplash.com/photo-1647452375084-3ad0db6c9331?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        listedDate: "2026-04-10",
+        agent: {
+            name: "Jack Reacher",
+            phone: "+44 7700 900001",
+            email: "jack.reacher@estateuk.com"
         }
 
     }
