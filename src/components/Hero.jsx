8b850a56b17ba6b268bc4be370b2f3f6577c9f94
acquah-forever <kojoaddo598@gmail.com>
2026-04-22 14:19:17 +0000

    git add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,10 +13,7 @@ const Hero = () => {
         <h1 className='text-5xl sm:text-6xl md:text-7xl text-amber-200'>Find Your Dream Home</h1>
         <h2 className='px-7 text-2xl sm:text-3xl text-white'>We understand the fact that mordern people strive for maximum comfort</h2>
       </div>
-  
-          <HashLink to='/#form' className="mt-10 text-white bg-linear-to-br from-pink-400 to-pink-700 px-5 sm:px-7 md:px-10 py-4 rounded-md text-xl sm:text-2xl md:text-3xl">Get Free Appointment</HashLink>
-        
-
+      <HashLink to='/#form' className="mt-10 text-white bg-linear-to-br from-pink-400 to-pink-700 px-5 sm:px-7 md:px-10 py-4 rounded-md text-xl sm:text-2xl md:text-3xl">Get Free Appointment</HashLink>
     </div>
   )
 }
