36c2fab156d004e666af005b33c2e043c8665437
acquah-forever <kojoaddo598@gmail.com>
2026-04-24 23:18:45 +0000

    moved hero button to new div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,16 @@ import { HashLink } from 'react-router-hash-link'
 const Hero = () => {
 
   return (
-    <div id='hero' className='relative min-h-screen flex flex-col justify-start text-center bg-cover bg-center bg-fixed overflow-hidden' >
+    <div id='hero' className='relative min-h-screen flex flex-col justify-start  text-center bg-cover bg-center bg-fixed overflow-hidden' >
       <div className='absolute inset-0 bg-black/20 z-0'></div>
       <div className='relative z-10 text-white text-left space-y-10'>
         <h1 className='text-5xl sm:text-6xl md:text-7xl text-amber-200'>Find Your Dream Home</h1>
-        <h2 className='px-7 text-2xl sm:text-3xl text-white'>We understand the fact that mordern people strive for maximum comfort</h2>
+        <h2 className='text-2xl sm:text-3xl text-white'>We understand the fact that mordern people strive for maximum comfort</h2>
+        <div className='mt-15 w-95 hover:scale-110 transition-all ease-in-out duration-300'>
+          <HashLink to='/#form' className="relative z-10 mt-10 text-white bg-linear-to-br from-green-400 to-green-700 px-5 sm:px-7 md:px-10 py-4 rounded-md text-xl sm:text-2xl md:text-3xl">Get Free Appointment</HashLink>
+        </div>
       </div>
-      <div className='mt-15 w-95 hover:scale-110 transition-all ease-in-out duration-300'>
-        <HashLink to='/#form' className="relative z-10 mt-10 text-white bg-linear-to-br from-green-400 to-green-700 px-5 sm:px-7 md:px-10 py-4 rounded-md text-xl sm:text-2xl md:text-3xl">Get Free Appointment</HashLink>
-      </div>
+
     </div>
   )
 }
